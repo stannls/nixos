a4fea01526626b2e311405d46758d2ecb7ab0958
Yannis Storrer <yannis.storrer@web.de>
2024-04-13 13:05:45 +0200

    Removed unused pkg direcroy

--- pkgs/default.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: let
-  callPackage = pkgs.callPackage;
-in {
-  nixpkgs.overlays = [(final: prev: {
-    mypackages = {
-      dracula = callPackage ./dracula.nix {};
-    };
-  })];
-}
