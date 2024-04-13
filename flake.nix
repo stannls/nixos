35dcb98d7255dc0da732226ae417acf3c93eed42
Yannis Storrer <yannis.storrer@web.de>
2024-04-13 14:19:02 +0200

    Nix-ld

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	nix-ld.url = "github:Mic92/nix-ld";
+	nix-ld.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       # The `follows` keyword in inputs is used for inheritance.
@@ -14,10 +16,11 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, nix-ld, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+	    nix-ld.nixosModules.nix-ld
         ./nixos/configuration.nix
         home-manager.nixosModules.home-manager
           {
