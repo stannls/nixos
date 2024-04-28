8e64916dec5874cf17fd19db168712322b2071e2
Yannis Storrer <yannis.storrer@web.de>
2024-04-29 00:58:09 +0200

    Fixed string error

--- home/beets/beets.nix
+++ home/beets/beets.nix
@@ -2,9 +2,9 @@
 {
 	programs.beets.enable = true;	
 	programs.beets.settings = {
-		directory = ~/Music/Library;
-		library = ~/Music/musiclibrary.db;
-		import.move = yes;
-		chroma.auto = yes;
+		directory = "~/Music/Library";
+		library = "~/Music/musiclibrary.db";
+		import.move = "yes";
+		chroma.auto = "yes";
 	};
 }
