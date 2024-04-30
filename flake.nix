9b2e730cc02873de50e5ed490ba994dbc76ab618
Yannis Storrer <yannis.storrer@web.de>
2024-04-30 22:57:59 +0200

    Fix syntax error in flake

--- flake.nix
+++ flake.nix
@@ -42,35 +42,34 @@
         }
       ];
     };
-  };
-  nixosConfigurations.laptop = nixpkgs.lib.nixosSystem rec {
-    system = "x86_64-linux";
-    specialArgs = { inherit self system; };
-    modules = [
-      # Dynamic linking shenanigans
-      ({ self, system, ... }: {
-        environment.systemPackages = with self.inputs.nix-alien.packages.${system}; [
-          nix-alien
-        ];
-        programs.nix-ld.enable = true;
-      })
-      nix-ld.nixosModules.nix-ld
 
-      # Nixos config
-      ./nixos/default.nix
-      ./nixos/laptop/configuration.nix
-      # Home manager config
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.extraSpecialArgs = {
-          inherit nix-colors;
-        };
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.users.yannis = import ./home/home.nix;
-      }
-    ];
-  };
-};
+    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem rec {
+      system = "x86_64-linux";
+      specialArgs = { inherit self system; };
+      modules = [
+        # Dynamic linking shenanigans
+        ({ self, system, ... }: {
+          environment.systemPackages = with self.inputs.nix-alien.packages.${system}; [
+            nix-alien
+          ];
+          programs.nix-ld.enable = true;
+        })
+        nix-ld.nixosModules.nix-ld
 
+        # Nixos config
+        ./nixos/default.nix
+        ./nixos/laptop/configuration.nix
+        # Home manager config
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.extraSpecialArgs = {
+            inherit nix-colors;
+          };
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.yannis = import ./home/home.nix;
+        }
+      ];
+    };
+  };
 }
