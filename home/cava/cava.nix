f51dd05a8fa4c1f52b6e4393d6c792d58dd9dedf
Yannis Storrer <yannis.storrer@web.de>
2024-05-01 01:25:04 +0200

    Fix gradient colors

--- home/cava/cava.nix
+++ home/cava/cava.nix
@@ -6,14 +6,14 @@
     color = {
       gradient = 1;
       gradient_count = 8;
-      gradient_color_1 = "#9967c4";
-      gradient_color_2 = "#9967c4";
-      gradient_color_3 = "#7aa2f7";
-      gradient_color_4 = "#7aa2f7";
-      gradient_color_5 = "#f7768e";
-      gradient_color_6 = "#f7768e";
-      gradient_color_7 = "#f7768e";
-      gradient_color_8 = "#f7768e";
+      gradient_color_1 = "'#9967c4'";
+      gradient_color_2 = "'#9967c4'";
+      gradient_color_3 = "'#7aa2f7'";
+      gradient_color_4 = "'#7aa2f7'";
+      gradient_color_5 = "'#f7768e'";
+      gradient_color_6 = "'#f7768e'";
+      gradient_color_7 = "'#f7768e'";
+      gradient_color_8 = "'#f7768e'";
 
     };
   };
